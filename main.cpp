9149d13e1b6b4d51796e3ebb1c94fa2d96f0c8ba
yagosalvador <37965771+yagosalvador@users.noreply.github.com>
2018-05-14 22:36:01 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -37,22 +37,22 @@ static void opt_help(string const &);
 //          parser,
 //          flag}
 //
+
 static option_t options[] = {
     {1, "i", "input", "-", opt_input, OPT_DEFAULT},
     {1, "o", "output", "-", opt_output, OPT_DEFAULT},
-    {1, "m", "method", "DFT", opt_method, OPT_DEFAULT},
+    {1, "m", "method", "FFT", opt_method, OPT_DEFAULT},
     {0, "h", "help", NULL, opt_help, OPT_DEFAULT},
     {0, },
 };
 
 static istream *iss = 0;
 static ostream *oss = 0;
-typedef Vector<Complex> (* transform_method)(const Vector<Complex> &v);
-static transform_method transform;
+typedef Vector<Complex> ( *transform_method)(const Vector<Complex> &v);
+//static transform_method transform;
 static fstream ifs;
 static fstream ofs;
-
-
+FourierTransform * FT;
 static void opt_input(string const &arg) {
     if (arg == "-") {
         iss = &cin;
@@ -87,15 +87,27 @@ static void opt_output(string const &arg) {
     }
 }
 
-static void opt_method(string const &method) {
-    if (method == "DFT") {
-        transform = DFT::transform;
-    } else if (method == "IDFT") {
-        transform = DFT::inverse;
-    } else {
-        cerr << ERROR_MSG_UNKNOWN_METHOD << endl;
-        opt_help("");
-    }
+static void opt_method(string const & method) {
+	    if (method == "DFT"){
+        	FT = new DFT();
+        	return;
+        }
+	    if (method == "IDFT"){
+        	FT = new IDFT();
+        	return;
+        }
+        if(method == "FFT"){
+	    	FT = new FFT();
+	    	return;
+	    }
+	    if(method == "IFFT"){
+	    	FT = new IFFT();
+	    	return;
+	    }
+		else{
+        	cerr << ERROR_MSG_UNKNOWN_METHOD << endl;
+           	opt_help("");
+		}
 }
 
 static void opt_help(string const &arg) {
@@ -107,40 +119,30 @@ static void opt_help(string const &arg) {
 int main(int argc, char * const argv[]) {
     cmdline cmdl(options);
     cmdl.parse(argc, argv);
-
     Vector<Complex> v;
     // std::vector< std::complex<double> > y;
-    clock_t begin = clock();
-
+    /*clock_t begin = clock();
+	*/
     if ((*iss >> v).bad()) {
         cerr << ERROR_MSG_CORRUPTED_DATA << endl;
     }
     // for (std::complex<double> t; *iss >> t;)
     //    y.push_back(t);
 
-    clock_t end = clock();
+    /*clock_t end = clock();
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     cout << "Tardó " << elapsed_secs << "segundos" << endl;
     std::cout << "Cantidad de elementos: " << v.getSize() << std::endl;
 
     begin = clock();
-    DFT::fft(v);
+    p->transform(v);
 
     end = clock();
     elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     cout << "FFT tardó " << elapsed_secs << "segundos" << endl;
 
-    //v[S,1,5,2];
-    //*oss << DFT::ditfft2(v) << endl;
-    Vector<Complex> c;
-    c.append(Complex(1,0));
-    c.append(Complex(1,0));
-    c.append(Complex(1,0));
-    c.append(Complex(1,0));
-    // *oss << "v = " << v << endl;
-    // Vector<Complex> whatever = DFT::fft(v);
-    // *oss << "fft(v) = " << whatever << endl;
-    // *oss << "ifft = " << DFT::ifft(whatever) << endl;
-
+    v[S,1,5,2];*/
+    //*oss << transform(v) << endl;
+    *oss << FT->transform(v) << endl;
     return 0;
 }
